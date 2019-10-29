1ab0a2315d491868306f49acb523eedf901ab0be
Tyler <gasperlint@gmail.com>
2019-10-28 21:48:58 -0700

    methods

--- Program.cs
+++ Program.cs
@@ -1,20 +1,45 @@
-﻿using System;
+using System;
 
 namespace IntermediateCSharp
 {
+
 
     class Program
     {
         static void Main(string[] args)
         {
-            var customer = new Customer();
-            customer.Id = 1;
-            customer.Name = "John";
-            var order = new Order();
-            customer.Orders.Add(order);
-            Console.WriteLine("id: " + customer.Id);
-            Console.WriteLine("name: " + customer.Name);
-            
+            int number;
+            var result = int.TryParse("abc", out number);
+            if(result)
+                Console.WriteLine(number);
+            else    
+                Console.WriteLine("Converion Fail");
+
+        }
+
+        static void UserParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1,2));
+            Console.WriteLine(calculator.Add(1,2,3,4));
+            Console.WriteLine(calculator.Add(new int[]{5,15,631,5}));
+
+        }
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10,20);
+                point.Move(new Point(40,60));
+                Console.WriteLine("Point is at {0}, {1}", point.X, point.Y);
+                point.Move(100,200);
+                Console.WriteLine("Point is at {0}, {1}", point.X, point.Y);
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Unexpected error occured");
+            }
         }
     }
+
 }
---
+++ Program3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IntermediateCSharp
+{
+
+    class Program3
+    {
+        static void Main3(string[] args)
+        {
+            var customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "John";
+            var order = new Order();
+            customer.Orders.Add(order);
+            Console.WriteLine("id: " + customer.Id);
+            Console.WriteLine("name: " + customer.Name);
+            
+        }
+    }
+}
